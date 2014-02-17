e818bea7229117192243566d91c05ae046c5e637
shouponlin <shoupon.lin@gmail.com>
2014-02-17 18:03:59 -0500

    Add filter to protect unauthenticated access to some pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    before_filter :require_user
+    protect_from_forgery with: :exception
+
+    def current_user
+        if @user
+            return @user
+        else
+            @user = User.find(session[:user_id]) if session[:user_id]
+        end
+    end
+    helper_method :current_user
+
+    def require_user
+        puts "Current_user (in require_user): "
+        puts current_user[:email] if current_user
+        puts "marker"
+        if current_user
+            return true
+        end
+        redirect_to login_url
+    end
 end
