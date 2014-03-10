d5b979d1f7b0b23c3a223502d9f745ab61f6d35b
shouponlin <shoupon.lin@gmail.com>
2014-03-10 16:34:47 -0400

    Added footer and login/logout partial views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,11 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
     def current_user
-        if @user
-            return @user
+        if @u && @u.id == session[:user_id]
+            return @u
         else
-            @user = User.find(session[:user_id]) if session[:user_id]
+            @u = User.find(session[:user_id]) if session[:user_id]
+            return @u
         end
     end
     helper_method :current_user
