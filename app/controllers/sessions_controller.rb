20e3797534aac95f36c909eb54a1b685a9ab02a6
shouponlin <shoupon.lin@gmail.com>
2014-04-02 16:53:01 -0400

    Make trip/index the welcome page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,9 @@ class SessionsController < ApplicationController
     skip_before_action :require_user, only: [:new, :create]
 
     def new
+      if session[:user_id]
+        redirect_to root_url
+      end
     end
 
     def create
@@ -18,6 +21,6 @@ class SessionsController < ApplicationController
 
     def destroy
         session[:user_id] = nil
-        redirect_to root_url
+        redirect_to new_session_url
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,7 @@ Demo::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'sessions#new'
+  root 'trips#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
